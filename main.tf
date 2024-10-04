359347e6842903b33e7b7839bd7abab4ecb48667
Taiwo Arowoyele <tarowoyele@gmail.com>
2024-10-04 17:20:32 +0200

    provider

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  default = "us-west-2"
+
+}
+
+provider "aws" {
+  region = var.aws_region
+
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-taiwo"
+
+
+  versioning {
+    enabled = true
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+
