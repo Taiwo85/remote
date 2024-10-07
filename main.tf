7f2b0029ae41a9f8fdd75b558306e9ce3346fd12
Taiwo Arowoyele <tarowoyele@gmail.com>
2024-10-07 21:19:55 +0200

    Versioning commented

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-taiwo"
 
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
   lifecycle {
     prevent_destroy = true
   }
