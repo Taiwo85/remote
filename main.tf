c11562be648846d9c089b33f8ae8869e3c0bf13c
Taiwo Arowoyele <tarowoyele@gmail.com>
2024-10-09 16:22:52 +0200

    variable

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-variable "aws_region" {
-  default = "us-west-2"
+# variable "aws_region" {
+#   default = "us-west-2"
 
-}
+# }
 
-provider "aws" {
-  region = var.aws_region
+# provider "aws" {
+#   region = var.aws_region
 
-}
+# }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-taiwo"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-state-taiwo"
 
 
-#   versioning {
-#     enabled = true
+# #   versioning {
+# #     enabled = true
+# #   }
+#   lifecycle {
+#     prevent_destroy = true
 #   }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+# }
---
+++ variable.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr_block" {}
+variable "availability_zone" {}
+variable "public_subnet_count" {}
+variable "private_subnet_count" {}
+variable "vpc_id" {}
