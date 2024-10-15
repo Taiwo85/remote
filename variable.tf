353d8be4f2ec8ff449aad838defdf5231d16b0d6
Taiwo Arowoyele <tarowoyele@gmail.com>
2024-10-15 22:58:33 +0200

    sns

--- variable.tf
+++ variable.tf
@@ -1,7 +1,8 @@
-variable "vpc_cidr_block" {}
-variable "availability_zone" {}
-variable "public_subnet_count" {}
-variable "private_subnet_count" {}
-variable "vpc_id" {}
-variable "public_subnet_ids" {}
-variable "availability_zone"{}
+# variable "vpc_cidr_block" {}
+# variable "availability_zone" {}
+# variable "public_subnet_count" {}
+# variable "private_subnet_count" {}
+# variable "vpc_id" {}
+# variable "public_subnet_ids" {}
+# variable "availability_zone"{}" {
+# Variable of sns
